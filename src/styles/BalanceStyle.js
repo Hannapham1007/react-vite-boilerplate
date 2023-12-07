8a8bf33c177c0faa307b34561283db41e40afb50
Hannapham1007 <hannaphamm1007@gmail.com>
2023-12-07 07:10:40 +0700

    add styling

--- src/styles/BalanceStyle.js
+++ src/styles/BalanceStyle.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
-export const IncomeStyle = styled.h2`
+export const IncomeStyle = styled.h3`
   color: var(--primary);
   margin-top: 4px;
   margin-bottom: 4px;
 `;
-export const ExpenseStyle = styled.h2`
+export const ExpenseStyle = styled.h3`
   color: var(--secondary);
   margin-top: 4px;
   margin-bottom: 4px;
 `;
-export const BalanceStyle = styled.h2`
+export const BalanceStyle = styled.h3`
   color: green;
   margin-top: 4px;
   margin-bottom: 4px;
